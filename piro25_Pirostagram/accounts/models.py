c7bf45a01baf9ab32b5f890a86f52e3b37aa191e
wepe37 <wpshs6058@gmail.com>
2026-07-08 21:52:00 +0900

    다양한 기능 구현, 마이페이지 게시글 수정중

--- piro25_Pirostagram/accounts/models.py
+++ piro25_Pirostagram/accounts/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    image = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+
 class Follow(models.Model):
     follower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following_set')
     following = models.ForeignKey(User, on_delete=models.CASCADE, related_name='follower_set')
