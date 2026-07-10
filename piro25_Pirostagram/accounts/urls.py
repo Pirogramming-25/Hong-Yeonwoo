25a12ff1f71cf280a1cb9392c1c7b3b8da815848
wepe37 <wpshs6058@gmail.com>
2026-07-10 10:50:03 +0900

    fix: add login page

--- piro25_Pirostagram/accounts/urls.py
+++ piro25_Pirostagram/accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/<str:username>/', views.profile, name='profile'),
     path('search/', views.user_search, name='user_search'),
     path('follow/<str:username>/', views.toggle_follow, name='toggle_follow'),
