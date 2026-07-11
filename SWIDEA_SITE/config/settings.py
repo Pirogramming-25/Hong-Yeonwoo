a1c9b2fef2a9bb0de5bb66885ea88bec81d9ad29
wepe37 <wpshs6058@gmail.com>
2026-07-11 16:14:13 +0900

    배포 실습

--- SWIDEA_SITE/config/settings.py
+++ SWIDEA_SITE/config/settings.py
@@ -21,12 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zphy4@7%9+gph^$b1p(40x(zc9em3+5f-fgz7#md9ko4vk7apb'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
